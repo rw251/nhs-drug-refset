3515b58d247735c1cc283a5179f4d745e740f7c0
Richard Williams <1234richardwilliams@gmail.com>
2023-11-29 13:52:16 +0000

    Responsive design changes

--- web/script.js
+++ web/script.js
@@ -9,13 +9,15 @@ const $refSetCodes = document.querySelector('#ref-set-codes tbody');
 const $title = document.querySelector('#ref-set-codes h3');
 const $header = document.querySelector('.header');
 const $message = document.getElementById('message');
-const $copy = document.querySelector('button');
+const $copy = document.getElementById('copy');
+const $tab = document.querySelector('.tab');
 
 const worker = new Worker('/web/worker.js');
 const loader =
   '<div class="lds-facebook"><div></div><div></div><div></div></div>';
 
 let data;
+let refSetTimer;
 
 worker.onmessage = (e) => {
   const { msg, content } = e.data;
@@ -25,14 +27,16 @@ worker.onmessage = (e) => {
       console.log('definitions loaded');
       break;
     case 'refsLoaded':
+      clearTimeout(refSetTimer);
       console.log('refs loaded');
       $listGroup.innerHTML = content.refSetHTML;
       $list = $listGroup.querySelectorAll('li');
       $wrapper.style.display = 'grid';
       document.querySelector('.scrollable-refs').style.height = `${
-        window.innerHeight - elHeight($inp) - 2 //border of ul wrapper
+        window.innerHeight - elHeight($inp) - elHeight($tab) - 38 //border of ul wrapper (2) + banner (36)
       }px`;
       $versionWrapper.style.display = 'none';
+      filter_list();
       $inp.focus();
       break;
     case 'data': {
@@ -58,15 +62,39 @@ let list;
 
 async function setupRoutes() {
   const routes = await fetch('/web/routes.json').then((x) => x.json());
-  $versionPicker.innerHTML = `<option disabled selected>Please select SNOMED version</option>${routes.map(
-    (x) => `<option>${x}</option>`
-  )}`;
+  $versionPicker.innerHTML = `<option disabled selected>Please select SNOMED version</option>${routes
+    .sort((b, a) => {
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    })
+    .map((x) => {
+      const [region, description, version, date] = x.split('_');
+      let readableVersion = x;
+      if (
+        region === 'uk' &&
+        description.indexOf('sct') === 0 &&
+        version.match(/^[0-9]+\.[0-9]+\.[0-9]+$/) &&
+        date.match(/^[0-9]{14}Z$/)
+      ) {
+        readableVersion = `v${version} (${date.substring(
+          0,
+          4
+        )}/${date.substring(4, 6)}/${date.substring(6, 8)})`;
+      }
+      return `<option value="${x}">${readableVersion}</option>`;
+    })}`;
 }
 
 $versionPicker.addEventListener('change', async (event) => {
+  $wrapper.style.display = 'none';
+  $refSetCodesPanel.style.display = 'none';
   const folder = event.target.value;
   worker.postMessage({ action: 'load', params: { folder } });
-  $versionWrapper.innerHTML = `<div style="padding-top:10px">Loading the data for ${folder}</div>${loader}`;
+  refSetTimer = setTimeout(() => {
+    $versionWrapper.innerHTML = `<div style="padding-top:10px">Loading the data for ${folder}</div>${loader}`;
+    $versionWrapper.style.display = 'block';
+  }, 100);
 });
 
 setupRoutes();
@@ -97,13 +125,14 @@ $listGroup.addEventListener('click', (e) => {
   const refSetId = e.target.dataset.id;
   e.target.classList.add('selected');
 
+  openTab('Codes');
   $refSetCodesPanel.style.display = 'block';
 
   $title.innerText = refSetId;
   $refSetCodes.innerHTML = loader;
 
   document.querySelector('.scrollable-defs').style.height = `${
-    window.innerHeight - elHeight($header)
+    window.innerHeight - elHeight($header) - elHeight($tab) - 36 // 36 = header
   }px`;
 
   $copy.setAttribute('disabled', '');
@@ -127,3 +156,23 @@ $copy.addEventListener('click', async (e) => {
     }, 2000);
   }, Math.max(0, 500 - diff));
 });
+
+function openTab(tabName) {
+  if (window.getComputedStyle($tab).display === 'none') return;
+
+  $refSetWrapper.style.display = 'none';
+  $refSetCodesPanel.style.display = 'none';
+
+  const tablinks = document.getElementsByClassName('tablinks');
+
+  if (tabName === 'Codes') {
+    tablinks[0].classList.remove('active');
+    tablinks[1].classList.add('active');
+    $refSetCodesPanel.style.display = 'block';
+  }
+  if (tabName === 'Refsets') {
+    tablinks[0].classList.add('active');
+    tablinks[1].classList.remove('active');
+    $refSetWrapper.style.display = 'block';
+  }
+}
